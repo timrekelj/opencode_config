618104816a5632810895c15e35b564b64c449768
Tim Rekelj <hello@timrekelj.si>
2026-09-21 07:43:36 +0200

    chore: Remove zed-bell plugin

--- plugins/zed-bell.js
+++
@@ -1,9 +0,0 @@
-export const ZedBell = async () => {
-  return {
-    event: async ({ event }) => {
-      if (event.type === "session.idle" || event.type === "permission.asked") {
-        process.stdout.write("\x07");
-      }
-    },
-  };
-};
